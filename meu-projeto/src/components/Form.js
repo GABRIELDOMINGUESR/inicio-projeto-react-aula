8f29d6b86e1da8f22316826a0d6ad2efffa42bd0
Guilherme Rufino <110607952+rufinoguilherme633@users.noreply.github.com>
2023-08-03 08:41:27 -0300

    useStates Form

--- meu-projeto/src/components/Form.js
+++ meu-projeto/src/components/Form.js
@@ -3,26 +3,63 @@
 // submissão de um formulário. No contexto de um formulário, ao chamar e.preventDefault() dentro de uma função de tratamento de evento, 
 // você impede que o navegador envie os dados do formulário ao servidor, permitindo que você execute ações personalizadas, como validações
 //  ou atualizações no estado, antes de prosseguir com o comportamento padrão.
+
+
+//trabalhando com estados -> 
+import {useState} from 'react'
+
 function Form() {
     function cadastrarUsuario(e) {
         e.preventDefault()
+        console.log(name)
+        console.log(password)
+        console.log(`Usuário ${name} cadastrado com a senha ${password}`)
         console.log('cadastrou o usuario');
 
-
     }
-    return(
+
+    const [name,setName] = useState()
+    const [password,setSenha] = useState()   
+
+
+    //nome do atributo que vou resgatar 
+    // setName o que vou alterar para 
+    return (
         <>
             <h1>Meu cadastro</h1>
-            <form onSubmit= {cadastrarUsuario}>
+            <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome"></input>                    
+                    <label htmlFor="name">Nome</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Digite seu nome"
+                        onChange={(e) => setName(e.target.value)}
+                    />
                 </div>
+
                 <div>
-                    <input type="submit" value="cadastrar"></input>
+                    <label htmlFor="password">Senha</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Digite sua senha"
+                        onChange={(e) => setSenha(e.target.value)}
+                    />
+                </div>
+
+                <div>
+                    <input type="submit" value="cadastrar" />
                 </div>
             </form>
         </>
-    )
+    );
 }
 
-export default Form
+export default Form;
+
+
+
+
