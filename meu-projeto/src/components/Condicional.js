7e0cc442611b3c7fd241dd168e86488ca884f1a6
ingryd <you@example.com>
2023-08-04 12:39:37 -0300

    if na rederizacao

--- meu-projeto/src/components/Condicional.js
+++ meu-projeto/src/components/Condicional.js
@@ -10,13 +10,26 @@ const [userEmail,setUserEmail] = useState()
         console.log(userEmail)
     }
 
+    function limparEmail() {
+        setemail('')
+
+    }
+
     return (
         <>
         <h2>Cadastre o seu e-mail</h2>
         <form>
             <input type="text" placeholder='Digite seu email' onChange={(e)=> setemail(e.target.value)}></input>
             <button onClick={enviarEmail}>Enviar e-mail</button>
-            {userEmail}
+            {/* {userEmail} */}
+{/* se user email e veridadeiro cria uma div */}
+            {userEmail &&(
+                <div>o email é: {userEmail}
+                <button onClick={limparEmail}>LImpar eEmail</button>
+
+                </div>
+                
+           )} 
         </form>
         
         
