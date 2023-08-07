b983092fd6164a0456b2af7bfacefe38051eb3b1
ingryd <you@example.com>
2023-08-07 14:59:05 -0300

    criando o projeto do curso aula #17

---
+++ costs/src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <p>Costs</p>
+    </div>
+  );
+}
+
+export default App;
+
+// para esse projeto foram istalados as segui ntes dependencias
+//npm install json-server -> backend fake para fazer reqisicao com api
+//npm install react-icon
+//npm install react-router-dom
+//npm install uuid
