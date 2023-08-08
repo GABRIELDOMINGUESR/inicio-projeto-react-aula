03431bdcca12751f966d85acfb2803645775072d
ingryd <you@example.com>
2023-08-08 14:49:34 -0300

    navbar e Footer aul #19

--- costs/src/App.js
+++ costs/src/App.js
@@ -5,6 +5,8 @@ import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProjects'
 import Container from './components/layout/Container'
 import Navibar from "./components/layout/Navibar";
+import Projects from "./components/pages/Projects"
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
@@ -17,10 +19,10 @@ function App() {
         <Route path='/company' element={<Company/>}></Route>
         <Route path='/contact' element={<Contact/>}></Route>
         <Route path='/newproject' element={<NewProject/>}></Route>
-
+        <Route path='/projects' element={<Projects/>}></Route>
       </Routes>
       </Container>      
-      <p>Footer</p>
+      <Footer/>
     </Router>
   );
 }
@@ -29,6 +31,6 @@ export default App;
 
 // para esse projeto foram istalados as segui ntes dependencias
 //npm install json-server -> backend fake para fazer reqisicao com api
-//npm install react-icon
+//npm install react-icons
 //npm install react-router-dom
 //npm install uuid
