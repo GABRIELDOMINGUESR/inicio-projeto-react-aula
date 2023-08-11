bc05b333fa227c503a1e813399b420b5bbdbeba0
ingryd <you@example.com>
2023-08-11 15:19:12 -0300

    Curso React: Criando Dashboard - #26

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -1,17 +1,27 @@
 import Message from "../layout/Message";
 import { useLocation } from "react-router-dom";
-
+import styles from "./Projects.module.css";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton"
 function Projects() {
   const location = useLocation(); //  a partir de agora consigo pegar o history do NewProject
-  let message =''
-  if(location.state) {
-    message = location.state.message
+  let message = "";
+  if (location.state) {
+    message = location.state.message;
   }
   return (
-    <div>
-      <h1>Meus Projetos</h1>
-     {message &&<Message type="success" msg={message} />}
+    <div className={styles.project_container}>
+      {/* <h1>Meus Projetos</h1> */}
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        {/* <a href="#">novo projeto</a> */}
+        <LinkButton to="/newproject" text=" criar Novo Projeto"/>
 
+      </div>
+      {message && <Message type="success" msg={message} />}
+      <Container customClass="start">
+        <p>Projetos</p>
+      </Container>
     </div>
   );
 }
