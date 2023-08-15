e7b9d65da1665ca582616ca290997a18683b24f2
GUILHERME RUFINO CAMPOS <22282090@CFP127ALUNO.LOCAL>
2023-08-15 15:40:59 -0300

    Curso React: Atualização do projeto - #32 min 9:00

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -1,13 +1,14 @@
- import styles from "./Project.module.css";
+import styles from "./Project.module.css";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Loading from '../layout/Loading'
 import Container from '../layout/Container'
+import ProjectForm from "../projects/ProjectForm";
 function Project() {
   const { id } = useParams();
   console.log(id); // pegar id da url
   const [project, setProject] = useState([]);
-  const [showProjectForm,setshowProjectForm] = useState(false)
+  const [showProjectForm, setshowProjectForm] = useState(false)
   useEffect(() => {
     setTimeout(() => {
       fetch(`http://localhost:5000/projects/${id}`, {
@@ -25,16 +26,37 @@ function Project() {
     }, 3000)
   }, [id]);
 
+
+  function editPos(project) {
+    console.log(project)
+    if (project.budget < project.cost) {
+      //mensagem
+    }
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project)
+    }).then(resp => resp.json())
+      .then(data => { 
+        console.log(data)
+        setProject(data)
+        setshowProjectForm(false)
+      })
+      .catch(err => { console.log(err) })
+
+  }
   function toggleProjectForm() {
-setshowProjectForm(!showProjectForm)
+    setshowProjectForm(!showProjectForm)
   }
 
   return (
     <>
-          {project.name ? (
+      {project.name ? (
         <div className={styles.project_details}>
           <Container customClass="column">
-         
+
             <div className={styles.details_container}>
               <h1>Projeto: {project.name}</h1>
               <button className={styles.btn} onClick={toggleProjectForm}>
@@ -53,8 +75,10 @@ setshowProjectForm(!showProjectForm)
                   </p>
                 </div>
               ) : (
-               <div className={styles.project_form}>detalhes do projeto</div>
-              )}
+                  <div className={styles.project_info}>
+                    <ProjectForm handleSubmit={editPos} btnText={"Concluir edição"} projecData={project} />
+                  </div>
+                )}
             </div>
           </Container>
         </div>
