fff0cc5f8234efb04bd597c8ee86ff2e1a5763ec
Guilherme Rufino <110607952+rufinoguilherme633@users.noreply.github.com>
2023-08-22 11:27:54 -0300

    Router para Login

--- abcdown/src/App.tsx
+++ abcdown/src/App.tsx
@@ -1,36 +1,86 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import Navibar from './components/layout/Navibar/Navibar'
+// import Home from './components/pages/Home';
+// import Turmas from './components/pages/Turmas';
+// import Comunidade from './components/pages/Comunidade';
+// import Configuracoes from './components/pages/Configuracoes';
+// import Login from './components/pages/Login';
+// import Sidebar from './components/layout/Sidebar'
+
+// function App() {
+//   const divStyle: React.CSSProperties = {
+//     margin: '0',
+//     padding: '0',
+//     boxSizing: 'border-box', // Correção aqui
+//     backgroundColor: 'lightgray',
+//     display: 'flex',
+//     flexDirection: 'row',
+//     width: '100%',
+//      height: '100vh',
+//   };
+
+//   return (
+//     <div style={divStyle}>
+//       <Router>
+//         <Sidebar/>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/turmas" element={<Turmas />} />
+//           <Route path="/comunidade" element={<Comunidade />} />
+//           <Route path="/configuracoes" element={<Configuracoes />} />
+//           <Route path="/login" element={<Login/>} />
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
-import logo from './logo.svg';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Navibar from './components/layout/Navibar/Navibar'
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Turmas from './components/pages/Turmas';
 import Comunidade from './components/pages/Comunidade';
 import Configuracoes from './components/pages/Configuracoes';
-import Login from './components/pages/Configuracoes';
-import Sidebar from './components/layout/Sidebar'
+import Login from './components/pages/Login';
+import Sidebar from './components/layout/Sidebar';
 
 function App() {
   const divStyle: React.CSSProperties = {
     margin: '0',
     padding: '0',
-    boxSizing: 'border-box', // Correção aqui
+    boxSizing: 'border-box',
     backgroundColor: 'lightgray',
     display: 'flex',
     flexDirection: 'row',
     width: '100%',
-     height: '100vh',
+    height: '100vh',
   };
 
   return (
     <div style={divStyle}>
       <Router>
-        <Sidebar/>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/turmas" element={<Turmas />} />
-          <Route path="/comunidade" element={<Comunidade />} />
-          <Route path="/configuracoes" element={<Configuracoes />} />
-          <Route path="/login" element={<Login/>} />
+          {/* Rota com Sidebar */}
+          <Route
+            path="/"
+            element={
+              <>
+                <Sidebar />
+                <Outlet /> {/* Renderiza rotas aninhadas */}
+              </>
+            }
+          >
+            <Route index element={<Home />} />
+            <Route path="/turmas" element={<Turmas />} />
+            <Route path="/comunidade" element={<Comunidade />} />
+            <Route path="/configuracoes" element={<Configuracoes />} />
+          </Route>
+          {/* Rota sem Sidebar */}
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
@@ -38,3 +88,4 @@ function App() {
 }
 
 export default App;
+
